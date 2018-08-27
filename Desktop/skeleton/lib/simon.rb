67825139ff98c9e13963841cea16205e001edbda
anthony <anthonyltam@gmail.com>
2018-08-26 18:35:54 -0700

    Pass simon game specs

---
+++ Desktop/skeleton/lib/simon.rb
@@ -0,0 +1,56 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+
+  def play
+    take_turn until @game_over
+
+    if @game_over
+      game_over_message
+      reset_game
+    end
+
+  end
+
+  def take_turn
+    show_sequence
+    require_sequence
+    @sequence_length += 1
+    round_success_message
+  end
+
+  def show_sequence
+    add_random_color
+  end
+
+  def require_sequence
+
+  end
+
+  def add_random_color
+    @seq << COLORS.sample
+    # @sequence_length += 1
+  end
+
+  def round_success_message
+    p 'round was a success!'
+  end
+
+  def game_over_message
+    p "game over"
+  end
+
+  def reset_game
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+end
+
+end
